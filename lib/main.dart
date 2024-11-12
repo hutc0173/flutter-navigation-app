c0249d596417c2afba58951d85cb2fe883bc3e53
Sammy Hutchinson <hutc0173@algonquinlive.com>
2024-11-12 16:26:20 -0500

    Exclude .env file from Git tracking

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'screens/home.dart';
 import 'screens/data.dart';
 import 'screens/contact.dart';
@@ -15,15 +16,79 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Navigation App',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.teal,
+          brightness: Brightness.dark,
+        ),
+        textTheme: TextTheme(
+          titleLarge: GoogleFonts.oswald(
+            fontSize: 28,
+            fontWeight: FontWeight.bold,
+            color: Colors.tealAccent,
+          ),
+          bodyMedium: GoogleFonts.merriweather(
+            fontSize: 18,
+            color: Colors.white70,
+          ),
+        ),
       ),
       home: const NavigationScreen(),
     );
   }
 }
 
-class NavigationScreen extends StatelessWidget {
-  const NavigationScreen({super.key})
+class NavigationScreen extends StatefulWidget {
+  const NavigationScreen({super.key});
 
+  @override
+  State<NavigationScreen> createState() => _NavigationScreenState();
+}
 
+class _NavigationScreenState extends State<NavigationScreen> {
+  int currentPageIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+
+    final List<Widget> pages = [
+      HomeScreen(),
+      DataScreen(),
+      ContactScreen(),
+    ];
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter Navigation App'),
+      ),
+      body: pages[currentPageIndex],
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: currentPageIndex,
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPageIndex = index;
+          });
+        },
+        indicatorColor: Colors.amber,
+        destinations: const <Widget>[
+          NavigationDestination(
+            selectedIcon: Icon(Icons.home),
+            icon: Icon(Icons.home_outlined),
+            label: 'Home',
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(Icons.data_usage),
+            icon: Icon(Icons.data_usage_outlined),
+            label: 'Data',
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(Icons.contact_mail),
+            icon: Icon(Icons.contact_mail_outlined),
+            label: 'Contact',
+          ),
+        ],
+      ),
+    );
+  }
 }
---
+++ lib/screens/contact.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ContactScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'Contact Screen',
+        style: Theme.of(context).textTheme.titleLarge,
+      ),
+    );
+  }
+}
